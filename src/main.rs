1b691af40a3dc66fc932be9bce28e2501f985785
CjS77 <CjS77@users.noreply.github.com>
2020-12-01 09:34:16 +0200

    Driver

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+
 fn main() {
-    println!("Hello, world!");
+    let args = std::env::args().collect::<Vec<String>>();
+    let problem = args.get(1)
+        .map(|s| s.as_str())
+        .unwrap_or("None");
+    let result = match problem {
+        "day1a" => "Foo",
+        _ => "We haven't solved that yet"
+    };
+    println!("{}", result);
 }
