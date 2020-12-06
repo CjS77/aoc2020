e628ef0a5e8b68088010e41cc77b15cf73f7d9c8
CjS77 <CjS77@users.noreply.github.com>
2020-12-06 10:15:35 +0200

    Day 6 solutions

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use aoc2020::day2::{day2a, day2b};
 use aoc2020::day3::{day3a, day3b};
 use aoc2020::day4::{day4a, day4b};
 use aoc2020::day5::{day5a, day5b};
+use aoc2020::day6::{day6a, day6b};
 
 fn main() {
     let args = std::env::args().collect::<Vec<String>>();
@@ -18,6 +19,8 @@ fn main() {
         "day4b" => day4b(),
         "day5a" => day5a(),
         "day5b" => day5b(),
+        "day6a" => day6a(),
+        "day6b" => day6b(),
         _ => "We haven't solved that yet".to_string(),
     };
     println!("{}", result);
