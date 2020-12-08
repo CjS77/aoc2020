7012221b53fe4d4a5a089fda705476a19e237d1d
CjS77 <CjS77@users.noreply.github.com>
2020-12-08 06:55:05 +0200

    Day7 needs touch ups

---
+++ src/day7.rs
@@ -0,0 +1,95 @@
+use std::fs;
+use regex::Regex;
+use std::collections::HashMap;
+
+pub fn day7a() -> String {
+    let bags = read_data();
+    let mybag = "shiny gold";
+    bags.values().fold(0usize, |tot, b| {
+        println!("{:?}", b);
+        if b.can_ultimately_hold(mybag, &bags) { tot + 1} else { tot }
+    }).to_string()
+}
+
+pub fn day7b() -> String {
+    let bags = read_data();
+    let mybag = "shiny gold";
+    // let mybag = "dark olive";
+    count_bags(bags.get(mybag).unwrap(), &bags).to_string()
+}
+
+fn count_bags(bag: &Bag, set: &HashMap<String, Bag>) -> usize {
+    if bag.contains.is_empty() {
+        println!("{} is empty", bag.color);
+        return 0;
+    }
+    println!("Checking {} inner bags: {:?}", bag.contains.len(), &bag.contains);
+    let n = bag.contains.iter()
+        .fold(0usize, |tot, (n, b)| {
+            let inner_count = count_bags(set.get(b.as_str()).unwrap(), set);
+            println!("Counting for {} '{}' inner bags = {}", n, b, inner_count);
+            tot + n * (1 + inner_count)
+        });
+    println!("{} contains {}", bag.color, n);
+    n
+}
+
+#[derive(Debug)]
+struct Bag {
+    color: String,
+    contains: Vec<(usize, String)>,
+}
+
+impl Bag {
+    pub fn new(color: &str) -> Self {
+        Self {
+            color: color.to_string(),
+            contains: Vec::new(),
+        }
+    }
+
+    pub fn can_contain(&mut self, n: usize, bag: &str) {
+        self.contains.push((n, bag.to_string()))
+    }
+
+    pub fn can_ultimately_hold(&self, color: &str, set: &HashMap<String, Bag>) -> bool {
+        if self.contains.is_empty() {
+            return false;
+        }
+        self.contains.iter()
+            .any(|(_, b)| {
+                b.as_str() == color || set.get(b.as_str()).unwrap().can_ultimately_hold(color, set)
+            })
+    }
+}
+
+fn read_data() -> HashMap<String, Bag> {
+    let re = Regex::new(r"^(.*) bags contain (.*)\.$").unwrap();
+    let mut set = HashMap::new();
+    fs::read_to_string("assets/day7.txt")
+        .expect("Could not read file")
+        .split("\n")
+        .filter_map(|s| to_bag(s, &re))
+        .for_each(|b| { set.insert(b.color.clone(), b); });
+    set
+}
+
+fn to_bag(s: &str, re: &Regex) -> Option<Bag> {
+    let re2 = Regex::new(r"^\s*(\d*) (.*) bags?$").unwrap();
+    let caps = re.captures(s)?;
+    let color = caps.get(1).unwrap().as_str();
+    let mut bag = Bag::new(color);
+    let contains = caps.get(2).unwrap().as_str();
+    if contains == "no other bags" {
+        return Some(bag);
+    }
+    contains.split(",")
+        .for_each(|s| {
+            let cap = re2.captures(s).unwrap();
+            let n = cap.get(1).unwrap().as_str().parse::<usize>().unwrap();
+            let color = cap.get(2).unwrap().as_str();
+            bag.can_contain(n, color)
+        });
+    println!("{:?}", bag);
+    Some(bag)
+}
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use aoc2020::day3::{day3a, day3b};
 use aoc2020::day4::{day4a, day4b};
 use aoc2020::day5::{day5a, day5b};
 use aoc2020::day6::{day6a, day6b};
+use aoc2020::day7::{day7a, day7b};
 
 fn main() {
     let args = std::env::args().collect::<Vec<String>>();
@@ -21,6 +22,8 @@ fn main() {
         "day5b" => day5b(),
         "day6a" => day6a(),
         "day6b" => day6b(),
+        "day7a" => day7a(),
+        "day7b" => day7b(),
         _ => "We haven't solved that yet".to_string(),
     };
     println!("{}", result);
