c16864d088ec595c73e92e31ed8a04058371e76b
CjS77 <CjS77@users.noreply.github.com>
2020-12-17 11:21:19 +0200

    Day17b -- a bit of a mess

--- src/day17.rs
+++ src/day17.rs
@@ -1,6 +1,15 @@
 use crate::bits::read_data;
 
 pub fn day17a() -> String {
+    for i in -1000..=1000 {
+        let pos = Cube::index_to_coord(20, i);
+        let index = Cube::coord_to_index(20, pos);
+        assert_eq!(i, index, "index {}, pos {:?}", index, pos);
+    }
+    format!("Ok")
+}
+
+pub fn day17b() -> String {
     let data = read_data("assets/day17.txt");
     let cube = parse_data(data);
     let cube = (0..6).fold(cube, |mut cube, i| {
@@ -13,12 +22,6 @@ pub fn day17a() -> String {
     format!("{}", cube.count_active())
 }
 
-pub fn day17b() -> String {
-    let data = read_data("assets/day17.txt");
-
-    format!("{}", 1)
-}
-
 #[derive(Clone)]
 struct Cube {
     size: usize,
@@ -27,13 +30,13 @@ struct Cube {
 }
 
 fn parse_data(data: Vec<String>) -> Cube {
-    let mut cube = Cube::new(100);
+    let mut cube = Cube::new(30);
     data.into_iter().enumerate()
         .for_each(|(y, s)| {
             s.chars().enumerate()
                 .for_each(|(x, c)| {
                     if c == '#' {
-                        cube.set_cube((x as isize,y as isize ,0), true);
+                        cube.set_cube((x as isize, y as isize, 0, 0), true);
                     }
                 })
         });
@@ -42,7 +45,8 @@ fn parse_data(data: Vec<String>) -> Cube {
 
 impl Cube {
     pub fn new(size: usize) -> Self {
-        let max = Self::coord_to_index(size, ((size-1) as isize, (size-1) as isize, (size-1) as isize)) as usize + 1;
+        let max_i = size as isize + 1;
+        let max = Self::coord_to_index(size, (max_i, max_i, max_i, max_i)) as usize + 1;
         Cube {
             size,
             pos_locs: vec![false; max],
@@ -50,22 +54,24 @@ impl Cube {
         }
     }
 
-    pub fn coord_to_index(size: usize, pos: (isize, isize, isize)) -> isize {
-        let (x, y, z) = pos;
+    pub fn coord_to_index(size: usize, pos: (isize, isize, isize, isize)) -> isize {
+        let (x, y, z, w) = pos;
         let b = size as isize + 1;
-        let c = ((size + 1) * (size + 1)) as isize;
-        x + b * y + c * z
+        let c = b * b;
+        let d = c * b;
+        x + b * y + c * z + d * w
     }
 
-    pub fn index_to_coord(size: usize, idx: isize) -> (isize, isize, isize) {
-        let size = size as isize;
+    pub fn index_to_coord(size: usize, idx: isize) -> (isize, isize, isize, isize) {
+        let n = (size + 1) as isize;
         let mut idx = idx;
-        let x = idx % (size + 1);
-        idx /= size + 1;
-        let y = idx % (size + 1);
-        idx /= size + 1;
-        let z = idx;
-        (x, y, z)
+        let x = idx % n;
+        idx /= n;
+        let y = idx % n;
+        idx /= n;
+        let z = idx % n;
+        idx /= n;
+        (x, y, z, idx)
     }
 
     pub fn is_active(&self, index: isize) -> Option<bool> {
@@ -76,7 +82,7 @@ impl Cube {
         }
     }
 
-    pub fn set_cube(&mut self, pos: (isize, isize, isize), val: bool) -> Option<()> {
+    pub fn set_cube(&mut self, pos: (isize, isize, isize, isize), val: bool) -> Option<()> {
         let index = Self::coord_to_index(self.size, pos);
         if index < 0 {
             let index = -index as usize;
@@ -96,17 +102,19 @@ impl Cube {
         pc + nc
     }
 
-    pub fn count_neighbours(&self, pos: (isize, isize, isize)) -> Option<usize> {
+    pub fn count_neighbours(&self, pos: (isize, isize, isize, isize)) -> Option<usize> {
         let this = Self::coord_to_index(self.size, pos);
-        let (x, y, z) = pos;
+        let (x, y, z, w) = pos;
         let mut count = 0usize;
         for dx in -1..=1 {
             for dy in -1..=1 {
                 for dz in -1..=1 {
-                    let index = Self::coord_to_index(self.size,(x + dx, y + dy, z + dz));
-                    if index == this { continue; }
-                    // ignore oob
-                    if matches!(self.is_active(index), Some(true)) { count += 1; }
+                    for dw in -1..=1 {
+                        let index = Self::coord_to_index(self.size, (x + dx, y + dy, z + dz, w + dw));
+                        if index == this { continue; }
+                        // ignore oob
+                        if matches!(self.is_active(index), Some(true)) { count += 1; }
+                    }
                 }
             }
         }
@@ -126,7 +134,7 @@ impl Cube {
     fn apply_rules(&mut self, next: &mut Cube, index: isize) -> Option<()> {
         let pos = Self::index_to_coord(self.size, index);
         let nc = self.count_neighbours(pos)?;
-        // println!("{:?} - active: {}, neighbours: {}", pos, self.is_active(index)?, nc);
+        // println!("idx: {}, {:?} - active: {}, neighbours: {}", index, pos, self.is_active(index)?, nc);
         let new_state = if self.is_active(index)? {
             nc == 2 || nc == 3
         } else {
