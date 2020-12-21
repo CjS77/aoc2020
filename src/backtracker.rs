769d37eacbc7b488c542d0a557716ef150f0e862
CjS77 <CjS77@users.noreply.github.com>
2020-12-22 01:54:30 +0200

    Nearly there

--- src/backtracker.rs
+++ src/backtracker.rs
@@ -1,38 +1,132 @@
-use std::collections::HashMap;
-use std::hash::Hash;
+type MoveIter<M> = Box<dyn Iterator<Item=M>>;
 
-pub struct BackTracker<S, I>
-where
-    S: Eq + Hash,
+pub fn depth_first_iterator<S, M>(initial_state: S) -> Option<S>
+    where
+        S: State<Move=M>,
 {
-    states: HashMap<S, I>,
-}
+    let mut states: Vec<(S, _)> = Vec::new();
+    let mut moves = initial_state.get_moves::<MoveIter<M>>();
+    let mut current_state = initial_state;
 
-impl<S,I> BackTracker<S,I>
-where
-    S: State,
-    I: Iterator<Item=S>
-{
-    pub fn solve(&mut self, state: &S) -> Option<S> {
-        if let Some(steps) = self.states.get_mut(state) {
-            // If there are no more possibilities, then there's no solution
-            let next_state = steps.next()?;
-            if !next_state.is_valid() {
-
+    loop {
+        match moves.next() {
+            None => {
+                // println!("Backtracking. {} states in stack", states.len());
+                if states.is_empty() {
+                    return None;  // There's no solution
+                }
+                let (cs, mvs) = states.pop().unwrap();
+                current_state = cs;
+                moves = Box::new(mvs);
             }
-            if next_state.is_solved() {
-                return Some(next_state)
+            Some(next_move) => {
+                // println!("Forward. {} states in stack", states.len());
+                let next_state = match current_state.apply(&next_move) {
+                    None => continue, // next move
+                    Some(s) => s,
+                };
+                if next_state.is_solved() {
+                    return Some(next_state);
+                }
+                let last_state = std::mem::replace(&mut current_state, next_state);
+                states.push((last_state, moves));
+                moves = current_state.get_moves::<MoveIter<M>>();
             }
-            return self.solve(next_state);
         }
-        let possible_next_states = state.next_steps().into_iter();
-        self.states.insert(state, possible_next_states);
-        self.solve(state)
     }
 }
 
-pub trait State {
-    fn is_valid(&self) -> bool;
+pub trait State: Eq + Sized {
+    type Move;
     fn is_solved(&self) -> bool;
-    fn next_steps<I: IntoIterator<Item=Self>>(&self) -> I;
+    fn get_moves<I>(&self) -> Box<dyn Iterator<Item=Self::Move>> where I: Iterator<Item=Self::Move>;
+    fn apply(&self, next_move: &Self::Move) -> Option<Self>;
+}
+
+#[cfg(test)]
+mod test {
+    use crate::backtracker::{State, depth_first_iterator};
+    use std::fmt;
+
+    #[derive(Eq, PartialEq, Clone)]
+    struct Board {
+        squares: Vec<isize>,
+        pos: usize,
+        size: usize,
+        turn: usize,
+    }
+
+    impl Board {
+        // Panics if x0, y0 is not a valid position
+        pub fn new(size: usize, x0: usize, y0: usize) -> Self {
+            let mut squares = vec![-1; size * size];
+            let pos = x0 + y0 * size;
+            squares[pos] = 0;
+            Board { squares, pos, size, turn: 0 }
+        }
+    }
+
+    impl Default for Board {
+        fn default() -> Self {
+            Self::new(8, 0, 0)
+        }
+    }
+
+    impl fmt::Display for Board {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            for (i, moved) in self.squares.iter().enumerate() {
+                if *moved == -1 { write!(f, "| XX ")? } else { write!(f, "| {:2} ", *moved)? };
+                if (i + 1) % self.size == 0 { write!(f, "|\n")? }
+            }
+            Ok(())
+        }
+    }
+
+    impl State for Board {
+        type Move = (isize, isize);
+
+        fn is_solved(&self) -> bool {
+            self.squares.iter().all(|s| *s >= 0)
+        }
+
+        fn get_moves<I>(&self) -> Box<dyn Iterator<Item=Self::Move>> where I: Iterator<Item=Self::Move> {
+            Box::new(vec![
+                (2, 1),
+                (2, -1),
+                (-2, 1),
+                (-2, -1),
+                (1, 2),
+                (1, -2),
+                (-1, 2),
+                (-1, -2),
+            ].into_iter())
+        }
+
+        fn apply(&self, next_move: &Self::Move) -> Option<Self> {
+            let mut x = (self.pos % self.size) as isize;
+            let mut y = (self.pos / self.size) as isize;
+            x += next_move.0;
+            y += next_move.1;
+            if x < 0 || y < 0 || x >= self.size as isize || y >= self.size as isize {
+                return None;
+            }
+            let i = y as usize * self.size + x as usize;
+            if self.squares[i] >= 0 {
+                return None;
+            }
+            let mut next = self.clone();
+            next.turn += 1;
+            next.pos = i;
+            next.squares[i] = next.turn as isize;
+            Some(next)
+        }
+    }
+
+    #[test]
+    fn knight() {
+        let k0 = Board::new(5, 0, 0);
+        let result = depth_first_iterator(k0).unwrap();
+        println!("{}", result);
+        assert_eq!(&result.squares, &[0,13,18,7,24,5,8,1,12,17,14,19,6,23,2,9,4,21,16,11,20,15,10,3,22]);
+    }
 }
--- src/day20.rs
+++ src/day20.rs
@@ -1,5 +1,10 @@
 use crate::bits::read_data;
-use std::collections::HashMap;
+use crate::backtracker::{State, depth_first_iterator};
+use std::collections::{HashMap, HashSet};
+use std::iter::FromIterator;
+use std::slice::{Iter};
+use std::fmt::{Display, Formatter};
+use std::fmt;
 use itertools::Itertools;
 
 pub fn day20a() -> String {
@@ -13,7 +18,11 @@ pub fn day20a() -> String {
 
 pub fn day20b() -> String {
     let tiles = read_tiles();
-    let image = Image::build_image(tiles);
+    let corner = tiles.iter().find(|t| t.id == 2711).unwrap().clone();
+    println!("Corner:\n{}", corner);
+    let mut image = Image::new(tiles);
+    image.place_tile(&corner, true, 1, 0);
+    let image = depth_first_iterator(image).unwrap();
     format!("")
 }
 
@@ -34,12 +43,21 @@ const MONSTER: [&str; 3] = [
     " #  #  #  #  #  #   "
 ];
 
-#[derive(Clone, Copy, PartialEq, Eq)]
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
 pub struct Tile {
     id: usize,
     data: [char; 100],
 }
 
+impl fmt::Display for Tile {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for chunk in self.data[..].chunks(10) {
+            let s: String = chunk.iter().cloned().collect();
+            writeln!(f, "{}", s)?;
+        }
+        Ok(())
+    }
+}
 impl Tile {
     pub fn new(id: usize, s: &[String]) -> Tile {
         let mut data = ['.'; 100];
@@ -83,6 +101,7 @@ impl Tile {
 
     pub fn rotate(&self, dir: usize) -> Tile {
         match dir {
+            0 => self.clone(),
             1 => self.transpose().flip_x(),
             2 => self.flip_x().flip_y(),
             3 => self.transpose().flip_y(),
@@ -121,13 +140,44 @@ impl Tile {
         res
     }
 
+    pub fn find_matches_on_edge(&self, edge: usize, other: &HashSet<Tile>) -> Vec<Placement> {
+        let that_edge = (edge + 2) % 4;
+        let orientations = [
+            (false, 0),
+            (false, 1),
+            (false, 2),
+            (false, 3),
+            (true, 0),
+            (true, 1),
+            (true, 2),
+            (true, 3),
+        ];
+        let mut result = Vec::new();
+        for t in other {
+            for (flip, rot) in orientations.into_iter() {
+                let mut tile = t.rotate(*rot);
+                if *flip { tile = tile.flip_x(); }
+                if self.match_edge(edge, false, &tile, that_edge) {
+                    let p = Placement {
+                        id: t.id,
+                        pos: 0,
+                        flip: *flip,
+                        rot: that_edge,
+                    };
+                    result.push(p);
+                }
+            }
+        }
+        result
+    }
+
     pub fn get_all_matches(&self, others: &[Tile]) -> Vec<EdgeMatch> {
         (0..4).map(|edge| {
-                others.iter().filter(|&t| t != self)
-                    .map(|t| self.find_matching_edge(edge, t))
-                    .flatten()
-                    .collect::<Vec<EdgeMatch>>()
-            })
+            others.iter().filter(|&t| t != self)
+                .map(|t| self.find_matching_edge(edge, t))
+                .flatten()
+                .collect::<Vec<EdgeMatch>>()
+        })
             .flatten()
             .collect()
     }
@@ -141,7 +191,9 @@ impl Tile {
                     self.find_matching_edge(*i, t).is_empty()
                 })
         }).collect();
-        // println!("{} had these edges with no matches: {:?}", self.id, no_matches);
+        if no_matches.len() == 2 {
+            println!("{} had these edges with no matches: {:?}", self.id, no_matches);
+        }
         no_matches.len() == 2
     }
 }
@@ -155,37 +207,136 @@ pub struct EdgeMatch {
 }
 
 
+#[derive(Clone)]
+pub struct Placement {
+    id: usize,
+    pos: usize,
+    flip: bool,
+    rot: usize,
+}
+
+#[derive(PartialEq, Eq, Clone)]
 pub struct Image {
-    data: Vec<char>
+    size: usize,
+    tiles_left: HashSet<Tile>,
+    arrangement: Vec<Option<Tile>>,
 }
 
 impl Image {
-    pub fn new(n: usize) -> Self {
-        let size = n * 100;
-        Self { data: vec!['@'; size] }
+    pub fn new(tiles: Vec<Tile>) -> Self {
+        let n = (tiles.len() as f64).sqrt() as usize;
+        let map = HashSet::from_iter(tiles.into_iter());
+        Self {
+            size: n,
+            tiles_left: map,
+            arrangement: vec![None; n * n],
+        }
     }
 
-    fn build_image(tiles: Vec<Tile>) -> Image {
-        let mut image = Image::new(tiles.len());
-        let corners = find_corners(&tiles);
-        println!("Corners: {:?}", corners);
+    pub fn place_tile(&mut self, tile: &Tile, flip: bool, rot: usize, pos: usize) -> Option<()> {
+        let n = self.size;
+        if pos >= n * n {
+            println!("Placement out of bounds");
+            return None;
+        }
+        if self.arrangement[pos].is_some() {
+            println!("Placover existing tile");
+            return None;
+        }
+        let mut tile = self.tiles_left.take(tile)?;
+        if rot != 0 { tile = tile.rotate(rot); }
+        if flip {
+            tile = tile.flip_x();
+        }
+        self.arrangement[pos] = Some(tile);
+        Some(())
+    }
 
-        assert_eq!(corners.len(), 4);
-        let mut match_db = HashMap::<usize, Vec<EdgeMatch>>::new();
-        tiles.iter()
-            .for_each(|t| {
-                let matches = t.get_all_matches(&tiles);
-                match_db.insert(t.id, matches);
+    #[inline(always)]
+    fn xy_to_i(&self, x: usize, y: usize) -> usize {
+        self.size * y + x
+    }
+
+    #[inline(always)]
+    fn i_to_xy(&self, i: usize) -> (usize, usize) {
+        let x = i % self.size;
+        let y = i / self.size;
+        (x, y)
+    }
+
+    fn is_empty_in_dir(&self, i: usize, dir: usize) -> Option<usize> {
+        let i = i as isize;
+        let ni = match dir {
+            0 => i - self.size as isize,
+            1 => i + 1,
+            2 => i + self.size as isize,
+            3 => i - 1,
+            _ => unreachable!()
+        };
+        if ni < 0 || ni >= self.arrangement.len() as isize {
+            return None;
+        }
+        let ni = ni as usize;
+        match self.arrangement[ni] {
+            None => Some(ni),
+            Some(_) => None,
+        }
+    }
+}
+
+impl fmt::Display for Image {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for row_num in 0..self.size * 10 {
+            let row = (0..self.size).map(|block_x| {
+                let block_y = row_num / 10;
+                let block_i = block_y * self.size + block_x;
+                let j = row_num % 10;
+                match self.arrangement[block_i] {
+                    None => ".".repeat(10),
+                    Some(t) => (&t.data[j*10..j*10+10]).iter().cloned().collect(),
+                }
+            }).join(" ");
+            writeln!(f, "{}", row)?;
+            if (row_num+1) % 10 == 0 {
+                writeln!(f, "")?;
+            }
+        }
+        Ok(())
+    }
+}
+
+impl State for Image {
+    type Move = Placement;
+
+    fn is_solved(&self) -> bool {
+        self.arrangement.iter().all(|t| t.is_some())
+    }
+
+    fn get_moves<I>(&self) -> Box<dyn Iterator<Item=Placement>> where I: Iterator<Item=Placement> {
+        let mut moves = Vec::new();
+        self.arrangement.iter()
+            .enumerate()
+            .filter(|(_i, t)| t.is_some())
+            .for_each(|(i, t)| {
+                let t = t.as_ref().unwrap();
+                for edge in 0usize..=3 {
+                    if let Some(new_i) = self.is_empty_in_dir(i, edge) {
+                        let mut matches = t.find_matches_on_edge(edge, &self.tiles_left);
+                        matches.iter_mut().for_each(|p| p.pos = new_i);
+                        moves.append(&mut matches);
+                    }
+                }
             });
+        // println!("{} possible moves", moves.len());
+        Box::new(moves.into_iter())
+    }
 
-        // Find the top left corner -- this can be any corner tile, the image will just be
-        // rotated / flipped
-
-        let (corner, m) = match_db.iter().find(|(&id, &m)| m.len() == 2).unwrap();
-        // How much to rotate?
-        let index = m.iter().map(|m| m.edge).min().unwrap();
-        let corner = remove_tile(&corner)
-        image
+    fn apply(&self, p: &Placement) -> Option<Self> {
+        let tile = self.tiles_left.iter().find(|t| t.id == p.id)?;
+        let mut next_state = self.clone();
+        println!("{} tiles placed", self.arrangement.iter().filter(|t| t.is_some()).count());
+        println!("{}", self);
+        next_state.place_tile(tile, p.flip, p.rot, p.pos).map(|_| next_state)
     }
 }
 
